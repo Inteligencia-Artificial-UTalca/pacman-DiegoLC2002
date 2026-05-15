81dfe92f3e845016f02cd2c9dee5d42f8b9c8b67
DiegoLC2002 <diegolopezcastillo483@gmail.com>
2026-05-15 10:37:01 -0400

    Lab 9 : MsPacman US (2)
    Se uso el PacmanSimpleController como base para crear un utility system basado en 3 variables: hunger, fear y pillUtiliy

--- src/Game.cpp
+++ src/Game.cpp
@@ -23,6 +23,7 @@
 #include "FSMController.h"
 #include "PinkyBT.h"
 #include "MsPacmanBT.h"
+#include "MsPacmanUtilitySys.h"
 
 extern bool quick;
 extern bool nogui;
@@ -35,7 +36,7 @@ gv(std::make_unique<GameView>(std::vector<std::string>{"images/maze-a.png","imag
 	auto pacman=std::make_shared<MsPacMan>(gameState.getMaze().getPacmanStart());
 	gameState.addPacMan(pacman);
 	// pacmanControl=std::make_shared<KeyboardController>(pacman);
-	 pacmanControl=std::make_shared<SimplePacmanController>(pacman);
+	 pacmanControl=std::make_shared<MsPacmanUtilitySys>(pacman);
 	// pacmanControl=std::make_shared<PacmanController>(pacman);
 	// pacmanControl=std::make_shared<MsPacmanBT>(pacman);
 
--- src/MsPacmanUtilitySys.cpp
+++ src/MsPacmanUtilitySys.cpp
@@ -1,67 +1,31 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include "UtilityPacmanController.h"
-
-#include <cmath>
+#include "MsPacmanUtilitySys.h"
 #include <iostream>
+#include <cmath>
+#include <SDL2/SDL.h>
 
-UtilityPacmanController::UtilityPacmanController(
-    std::shared_ptr<Character> character)
-    : Controller(character)
+
+MsPacmanUtilitySys::MsPacmanUtilitySys(std::shared_ptr<Character> character):
+	Controller(character)
 {
 }
 
-UtilityPacmanController::~UtilityPacmanController()
+MsPacmanUtilitySys::~MsPacmanUtilitySys() 
 {
+	// TODO Auto-generated destructor stub
 }
 
-////////////////////////////////////////////////////////
-
-Move UtilityPacmanController::getClosestMove(
-    const GameState& game,
-    std::pair<int,int> target) const
+Move MsPacmanUtilitySys::getClosestMove(const GameState& game, std::pair<int,int> target)const
 {
-    int minDist = 1e9;
+	int minDist = 1e9;
+	Move bestMove = PASS;
 
-    Move bestMove = PASS;
-
-    auto moves =
-        game.getMaze().getPossibleMoves(character->getPos());
+	auto moves = game.getMaze().getPossibleMoves(character->getPos());
 
     for(auto move : moves)
     {
-        int next =
-            game.getMaze().getNeighbour(character->getPos(),
-                                        move);
+        int next = game.getMaze().getNeighbour(character->getPos(), move);
 
-        if(next < 0) continue;
+        if(next < 0) { continue; }
 
         auto pos = game.getMaze().getNodePos(next);
 
@@ -77,24 +41,18 @@ Move UtilityPacmanController::getClosestMove(
     return bestMove;
 }
 
-////////////////////////////////////////////////////////
 
-Move UtilityPacmanController::getFarthestMove(
-    const GameState& game,
-    std::pair<int,int> target) const
+Move MsPacmanUtilitySys::getFarthestMove(const GameState& game, std::pair<int,int> target)const
 {
-    int maxDist = -1;
+	int maxDist = -1;
 
     Move bestMove = PASS;
 
-    auto moves =
-        game.getMaze().getPossibleMoves(character->getPos());
+    auto moves = game.getMaze().getPossibleMoves(character->getPos());
 
     for(auto move : moves)
     {
-        int next =
-            game.getMaze().getNeighbour(character->getPos(),
-                                        move);
+        int next = game.getMaze().getNeighbour(character->getPos(), move);
 
         if(next < 0) continue;
 
@@ -112,24 +70,32 @@ Move UtilityPacmanController::getFarthestMove(
     return bestMove;
 }
 
-////////////////////////////////////////////////////////
 
-float UtilityPacmanController::getDistanceToGhost(
-    const GameState& game,
-    int ghost) const
+float MsPacmanUtilitySys::getDistanceToGhost(const GameState& game, int g)const
 {
-    return sqrt(
-        euclid2(
-            game.getMaze().getNodePos(character->getPos()),
-            game.getMaze().getNodePos(game.getGhostsPos(ghost))
-        )
-    );
+	return sqrt(euclid2(
+		game.getMaze().getNodePos(character->getPos()),
+		game.getMaze().getNodePos(game.getGhostsPos(g))));
 }
 
-////////////////////////////////////////////////////////
 
-Move UtilityPacmanController::getMove(const GameState& game)
+Move MsPacmanUtilitySys::getMove(const GameState& game)
 {
+    //para cerrar la ventana
+	SDL_Event e;
+	if( SDL_PollEvent( &e ) != 0 )
+	{
+		if( e.type == SDL_QUIT || 
+			(e.type == SDL_KEYDOWN && 
+				(e.key.keysym.sym==SDLK_ESCAPE || 
+				e.key.keysym.sym==SDLK_q) ))
+		{
+			SDL_Quit();
+			exit(0);
+		}
+	}
+
+    //Variables de utilidad
     float fear = 0.0f;
     Move escapeMove = PASS;
 
@@ -139,73 +105,54 @@ Move UtilityPacmanController::getMove(const GameState& game)
     float pillUtility = 0.3f;
     Move pillMove = PASS;
 
-    ////////////////////////////////////////////////////
-    // ESCAPE
-    ////////////////////////////////////////////////////
 
-    for(int i = 0; i < 4; i++)
+    //Escapar de fantasmas
+	for(int i = 0; i < 4; i++)
     {
-        if(game.isGhostEdible(i)) continue;
+        if(game.isGhostEdible(i)) { continue; } //Ignorar fantasmas comestibles
 
         float dist = getDistanceToGhost(game, i);
 
-        // logística
-        float utility =
-            1.0f /
-            (1.0f + exp((dist - 30.0f) * 0.15f));
+        // curava logística
+        float utility = 1.0f / (1.0f + exp((dist - 30.0f) * 0.15f));
 
         if(utility > fear)
         {
             fear = utility;
-
-            auto ghostPos =
-                game.getMaze().getNodePos(
-                    game.getGhostsPos(i));
-
-            escapeMove =
-                getFarthestMove(game, ghostPos);
+            auto ghostPos = game.getMaze().getNodePos(game.getGhostsPos(i));
+            escapeMove = getFarthestMove(game, ghostPos);
         }
-    }
+		
+	}
 
-    ////////////////////////////////////////////////////
-    // HUNT BLUE GHOSTS
-    ////////////////////////////////////////////////////
 
+    //Perseguir fantasmas azules
     for(int i = 0; i < 4; i++)
     {
-        if(!game.isGhostEdible(i)) continue;
+        if(!game.isGhostEdible(i)) { continue; }
 
         float dist = getDistanceToGhost(game, i);
 
         // cuadrática
-        float utility =
-            pow(std::max(0.0f, 80.0f - dist), 2)
-            / (80.0f * 80.0f);
+        //Usa max para evitar valores negativos y establecerlos en 0 si lo hace.
+        float utility = pow(std::max(0.0f, 80.0f - dist), 2) / (80.0f * 80.0f);
 
         if(utility > hunger)
         {
             hunger = utility;
 
-            auto ghostPos =
-                game.getMaze().getNodePos(
-                    game.getGhostsPos(i));
+            auto ghostPos = game.getMaze().getNodePos(game.getGhostsPos(i));
 
-            huntMove =
-                getClosestMove(game, ghostPos);
+            huntMove = getClosestMove(game, ghostPos);
         }
     }
 
-    ////////////////////////////////////////////////////
-    // PILLS
-    ////////////////////////////////////////////////////
-
+    //Comer pildoras
     auto pills = game.getMaze().getPillPositions();
-
     float minPillDist = 1e9;
     std::pair<int,int> bestPill;
 
-    auto pacPos =
-        game.getMaze().getNodePos(character->getPos());
+    auto pacPos = game.getMaze().getNodePos(character->getPos());
 
     for(auto pill : pills)
     {
@@ -222,28 +169,30 @@ Move UtilityPacmanController::getMove(const GameState& game)
     {
         pillMove = getClosestMove(game, bestPill);
 
-        // utilidad inversa
-        pillUtility =
-            1.0f / (1.0f + minPillDist * 0.01f);
+        // utilidad para pildoras (a menor distancia mayor utilidad)
+        pillUtility = 0.4f / (1.0f + minPillDist * 0.02f);
     }
 
-    ////////////////////////////////////////////////////
-    // DECISION
-    ////////////////////////////////////////////////////
+
+    //Comparar utilidades 
+    std::cout << "fear = " << fear << std::endl;
+    std::cout << "hunger = " << hunger << std::endl;
+    std::cout << "pill = " << pillUtility << std::endl;
 
     if(fear > hunger && fear > pillUtility)
     {
-        std::cout << "ESCAPE\n";
+        std::cout << "Estamos escapando.!!!\n";
         return escapeMove;
     }
 
     if(hunger > pillUtility)
     {
-        std::cout << "HUNT\n";
+        std::cout << "Vamos a comer fantasmas.!!!\n";
         return huntMove;
     }
 
-    std::cout << "PILLS\n";
+    std::cout << "Pastilllas Yumi.!!!\n";
+
 
     return pillMove;
 }
